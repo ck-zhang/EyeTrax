cb4ed361fcfe70347fd016dcebcc0d17368d0466
Vamsavardhana Vijay <vvamsava@gmail.com>
2026-02-07 17:35:05 +0530

    dense grid calibration - tested 10*10 grid, 5*5 grid

--- src/eyetrax/calibration/__init__.py
+++ src/eyetrax/calibration/__init__.py
@@ -1,7 +1,8 @@
-from .common import compute_grid_points, wait_for_face_and_countdown
+from .common import compute_grid_points, wait_for_face_and_countdown, compute_grid_points_from_shape
 from .five_point import run_5_point_calibration
 from .lissajous import run_lissajous_calibration
 from .nine_point import run_9_point_calibration
+from .dense_grid import run_dense_grid_calibration
 
 __all__ = [
     "wait_for_face_and_countdown",
